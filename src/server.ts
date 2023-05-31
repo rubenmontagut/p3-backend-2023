6ccae35ef549a3810991bd8c6b1a94366b942346
rubenmontagut <ruben.pinell01@gmail.com>
2023-05-31 21:38:26 +0200

    get restaurants

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 dotenv.config();
 console.log(process.env);
@@ -11,8 +14,15 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.get("/", async (req, res) => {
-  res.status(200).json("hi!");
+app.get("/restaurants", async (req, res) => {
+  try {
+    const result = await prisma.restaurant.findMany({});
+    res.status(200).json(result);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ type: error.constructor.name, message: error.toString() });
+  }
 });
 
 const { SERVER_PORT } = process.env;
